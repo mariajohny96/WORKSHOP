3ea9295efc5a55e4aa1822eed4e702676e6a55c9
mariajohny96 <johnymaria96@gmail.com>
2018-07-04 20:44:26 +0530

    hello

---
+++ string.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Anagrams
+{
+    int c = 0;
+     
+    void input()throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a word : ");
+        String s = sc.next();
+       
+    }
+
+ 
+
+   
+   
